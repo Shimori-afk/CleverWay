af19c43e72e21c1e094ed1034dc5c1d7168d6fb8
Shimori-afk <maksym.leonovych@gmail.com>
2024-03-19 04:08:35 +0200

    First commit

---
+++ CleverWay/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CleverWay
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CleverWay/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CleverWay
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
